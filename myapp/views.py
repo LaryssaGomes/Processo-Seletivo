2f9e971aa2bcd6ec97ee9b34ed1497a9b27d5175
LaryssaGomes <laryssag533@gmail.com>
2021-04-12 19:19:04 -0300

    add env

--- myapp/views.py
+++ myapp/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 
 # Create your views here.
 def home_page(request):
-    title_btn = 'Currículo de Laryssa'
+    title_btn = 'Currículo'
     return render(request, 'home.html', {'title_btn': title_btn})
 
 def curriculo(request):
