56707508aaa6bdae9df7b34e1834f356a93d2e81
LaryssaGomes <laryssag533@gmail.com>
2021-04-12 14:39:24 -0300

    first commit

---
+++ myapp/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+
+# Create your views here.
+def home_page(request):
+    title_btn = 'Currículo de Laryssa'
+    return render(request, 'home.html', {'title_btn': title_btn})
+
+def curriculo(request):
+    return render(request, 'curriculo.html', )
